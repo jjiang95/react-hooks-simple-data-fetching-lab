f521c35be6defffe46744fd8e628fb2d53839834
Jasmine Jiang <jasminemazzarella@gmail.com>
2023-06-01 19:38:06 -0400

    complete lab

--- src/components/App.js
+++ src/components/App.js
@@ -1,1 +1,30 @@
 // create your App component here
+import React, { useEffect, useState } from "react";
+
+function App() {
+
+    const [img, setImg] = useState("")
+    const [imgPresent, setImgPresent] = useState(false)
+    
+    useEffect(() => {
+        fetch("https://dog.ceo/api/breeds/image/random")
+        .then(resp => resp.json())
+        .then(data => {
+            setImg(data.message)
+            setImgPresent(true)
+        })
+    }, [])
+    
+    if (!imgPresent) {
+        return <p>Loading...</p>
+    }
+
+    return (
+        <>
+            <h1>Random Dog Generator</h1>
+            <img src={img} alt="A Random Dog"/>
+        </>
+    )
+}
+
+export default App;
